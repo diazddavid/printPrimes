bd4d85f0ec5d1664766187a047fedd1873cde1a4
diazddavid <diazrivasdavid5@gmail.com>
2018-01-02 22:22:16 +0100

    Primer camino principal

--- src/test/java/org/urjc/isi/travis/PrintPrimesTest.java
+++ src/test/java/org/urjc/isi/travis/PrintPrimesTest.java
@@ -7,10 +7,37 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class PrintPrimesTest {
+	
+	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+
+	@Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @After
+    public void cleanUpStreams() {
+        System.setOut(null);
+        System.setErr(null);
+    }
 
 	@Test
 	public void noPrimes() {
 		PrintPrimes.main(new String[] {String.valueOf(0)});
+	}
+	
+	@Test
+	public void noArg(){
+		String[] args = {};
+        String string = "word\nword\nword\n";
+		InputStream stringStream = new ByteArrayInputStream(string.getBytes());
+        System.setIn(stringStream);
+        
+        PrintPrimes.main(args);
+        assertEquals("Usage: java PrintPrimes v1 \n", outContent.toString());
+        System.setIn(System.in);
 	}
 
 }
